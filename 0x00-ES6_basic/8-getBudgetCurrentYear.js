7e5f7f75a7c6403579f0678973a0e25712c5da11
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-06-28 16:22:09 +0300

    Refactor getBudgetCurrentYear.js to use ES6 default export for getBudgetForCurrentYear function

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  
+  return {
+    [`${income}-${currentYear}`] : income,
+    [`${gdp}-${currentYear}`] : gdp,
+    [`${capita}-${currentYear}`] : capita,
+  };
+}
