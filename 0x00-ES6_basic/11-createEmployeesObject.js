c141b15d8caa7596315e4ba10229251a8f4ece63
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-06-28 16:22:48 +0300

    Refactor createEmployeesObject.js to use ES6 default export for createEmployeesObject function

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function createEmployeesObject(departmentName, employees) {
+    // Create an object with the departmentName as the key
+    // and the employees array as the value.
+    return {
+        [departmentName]: employees,
+    };
+}
