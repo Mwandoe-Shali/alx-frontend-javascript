a6afa7f64007f378bd22a4424632f7173765a39e
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-06-28 16:23:28 +0300

    Refactor iterateThroughObject.js to use ES6 default export for iterateThroughObject function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+
+/**
+ * This function takes an object as an argument and returns a string
+ * that contains all the properties of the object separated by ' | '.
+ * The last occurrence of ' | ' is removed from the result.
+ *
+ * @param {Object} reportWithIterator - The object to iterate through.
+ * @return {string} The string that contains all the properties of the object.
+ */
+export default function iterateThroughObject(reportWithIterator) {
+  // Initialize an empty string to store the result
+  let result = '';
+
+  // Get the first property of the object
+  let iteratorResult = reportWithIterator.next();
+
+  // Loop through all the properties of the object
+  while (!iteratorResult.done) {
+    // Append the current property to the result string
+    result += iteratorResult.value + ' | ';
+
+    // Get the next property of the object
+    iteratorResult = reportWithIterator.next();
+  }
+
+  // Remove the last occurrence of ' | ' from the result string
+  return result.slice(0, -3);
+}
