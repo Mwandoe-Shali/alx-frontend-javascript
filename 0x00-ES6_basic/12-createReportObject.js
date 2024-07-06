97e4313c097c1fbf822c59294a213e1606770b08
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-07-06 21:37:42 +0300

    made it es6 compliant

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 export default function createReportObject(employeesList) {
-    const allEmployees = { ...employeesList };
+  const allEmployees = { ...employeesList };
 
-    return {
-        allEmployees,
-        getNumberOfDepartments() {
-            return Object.keys(employeesList).length;
-        }
-    };
-
+  return {
+    allEmployees,
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
+    }
 }
