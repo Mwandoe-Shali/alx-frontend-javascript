901c0ea275f68050b7e2b084942ad3b28410ccab
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-07-06 19:49:10 +0300

    changed indentation

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-    this.addNeighborhood = (newNeighborhood) => {
-        this.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return this.sanFranciscoNeighborhoods;
-    };
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
 }
