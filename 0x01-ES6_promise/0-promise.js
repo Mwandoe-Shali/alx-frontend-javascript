ca7d10b07b51085493d7e048c21a18ddde4349da
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-08-09 12:37:16 +0300

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        resolve(data);
+        reject(error); 
+    });
+}
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+export default function getFullResponseFromAPI(success) {
+    return new Promise ((resolve, reject) => {
+        const object = {
+            status: 200,
+            body: 'Success',
+        };
+
+        if (success) {
+            resolve(object)
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
