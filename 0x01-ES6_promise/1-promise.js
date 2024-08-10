6a804dda4dc4b18b0e7ac2da6ee2933675bf26c4
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-08-10 07:34:00 +0300

    fix lint errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 export default function getFullResponseFromAPI(success) {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const object = {
       status: 200,
       body: 'Success',
     };
 
     if (success) {
-      resolve(object)
+      resolve(object);
     } else {
       reject(new Error('The fake API is not working currently'));
     }
