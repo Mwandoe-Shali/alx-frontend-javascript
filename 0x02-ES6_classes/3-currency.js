6e3fe97c70d9a99c5faf046fed051104a2b62c42
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-11-28 23:44:39 +0300

    back baby

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+
+export default class Currency {
+    constructor(code, name) {
+      this.code = code;  // Use setter for validation
+      this.name = name;  // Use setter for validation
+    }
+
+    // Getter and Setter for 'code'
+    get code() {
+      return this._code;
+    }
+
+    set code(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      this._code = value;
+    }
+
+    // Getter and Setter for 'name'
+    get name() {
+      return this._name;
+    }
+
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = value;
+    }
+
+    // Method to display the full currency
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
+  }
