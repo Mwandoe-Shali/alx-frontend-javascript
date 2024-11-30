1eb5691c59b066464d762a92be547f561e253994
Mwandoe Shali. <117778682+Mwandoe-Shali@users.noreply.github.com>
2024-11-30 19:14:32 +0300

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,3 +50,17 @@ function createEmployee(salary: number | string): Director | Teacher {
         return new Director();
     }
 }
+
+// isDirector Function
+function isDirector(employee: any): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// executeWork Function
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
